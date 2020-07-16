744593b47e739a9722a3a1ccce469ba6cd181342
JunKudo0222 <jkddr3231@gmail.com>
2020-07-16 21:57:01 +0900

    add search method

--- resources/views/gyms/index.blade.php
+++ resources/views/gyms/index.blade.php
@@ -6,13 +6,22 @@
         <div class="col-md-10">
 	         <div class="card text-center">
 			<div class="card-header">
-				体育館一覧
+				<a href="{{ route('gyms.index') }}">体育館一覧</a>
 			</div>
 			<div class="card-body">
 				<form action="{{route('gyms.search')}}" method="get">
 					@csrf
 					<p>検索したいキーワードを入力してください。</p>
-  					<input type="search" name="search" placeholder="キーワードを入力">
+  					<input type="search" name="search" placeholder="キーワードを入力"><br>
+					  
+                    
+                    @foreach($weekdays as $weekday)
+                        <input type="checkbox" class="" id="{{ $weekday ->id }}" name="weeks[]" value="{{$weekday -> id}}">
+                        <label class="" for="{{ $weekday -> id }}">
+                            {{ $weekday -> name }}
+                        </label>
+                    @endforeach
+                
   					<input type="submit"  value="検索">
 				</form>
 				@isset($search_result)
@@ -31,11 +40,18 @@
 				<div class="card col-md-3" style="width: 18rem;">
 				
   
-  <div class="card-body">
+  <div class="card-body" class="text-center">
     <h5 class="card-title">{{$gym -> name }}</h5>
+	@if(isset($gym->image_path))
+	<img src="{{ $gym->image_path }}" alt="画像" class="col-12">
+	@endif
     <p class="card-text">{{$gym -> prefecture ->name}}</p>
     <p class="card-text">{{$gym -> area ->name}}</p>
     <p class="card-text">{{$gym -> price}}</p>
+    <p class="card-text">@foreach( $gym->weekdays as $weekday)
+                    {{ $weekday->name }}/
+                @endforeach</p>
+
     <a href="{{ route('gyms.show',$gym ->id) }}" class="btn btn-primary">詳細へ</a>
   </div>
 </div>
